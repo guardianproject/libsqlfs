adaa2e11b5bd1ddd8965ee2b09063f021698b46f
Hans-Christoph Steiner <hans@eds.org>
2012-03-15 15:23:12 -0400

    #include <stdlib.h> for malloc() to get rid of warning

--- tests/test.c
+++ tests/test.c
@@ -19,6 +19,7 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
 
 #include <assert.h>
 #include <string.h>
+#include <stdlib.h>
 #include "sqlfs.h"
 
 int main(int argc, char *argv[])

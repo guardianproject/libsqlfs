f5bf76cdfd7ca609e0b61fcca785d050854c9500
Hans-Christoph Steiner <hans@eds.org>
2012-03-30 11:34:26 -0400

    added more tests to SQLite test program and cleaned up building of tests

--- tests/test.c
+++ tests/test.c
@@ -24,6 +24,23 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
 #include <time.h>
 #include "sqlfs.h"
 
+int exists(char *filename)
+{
+    FILE *file;
+    if ((file = fopen(filename, "r")) == NULL) {
+        if (errno == ENOENT) {
+            printf("File doesn't exist");
+        } else {
+            // Check for other errors too, like EACCES and EISDIR
+            printf("Some other error occured");
+        }
+        return 0;
+    } else {
+        fclose(file);
+    }
+    return 1;
+}
+
 int main(int argc, char *argv[])
 {
     char *filename = "test.db";
@@ -38,17 +55,24 @@ int main(int argc, char *argv[])
 
     if(argc > 1)
       filename = argv[1];
+    if(exists(filename))
+       printf("%s exists.\n", filename);
     printf("Opening %s\n", filename);
     sqlfs_init(filename);
+    printf("Running tests:\n");
+
     rc = sqlfs_open(filename, &sqlfs);
+    printf("Opening database...");
     assert(rc);
+    printf("passed\n");
 
     sqlfs_proc_mkdir(sqlfs, "/test", 0777);
     assert(sqlfs_is_dir(sqlfs, "/test"));
     sqlfs_proc_mkdir(sqlfs, "/test/1", 0777);
-    //assert(sqlfs_is_dir(sqlfs, "/test/1"));
+    assert(sqlfs_is_dir(sqlfs, "/test/1"));
     sqlfs_proc_mkdir(sqlfs, "/test/2", 0777);
-    //assert(sqlfs_is_dir(sqlfs, "/test/2"));
+    sleep(1);
+    assert(sqlfs_is_dir(sqlfs, "/test/2"));
     sqlfs_proc_mkdir(sqlfs, "/a/b/c/d/e/f/g", 0777);
 
     fi.flags |= ~0;
@@ -58,27 +82,30 @@ int main(int argc, char *argv[])
     assert(sqlfs_is_dir(sqlfs, "/test"));
     i = sqlfs_proc_read(sqlfs, "/test/2/file", buf, sizeof(buf), 0, &fi);
     buf[i] = 0;
-    printf("buf(%s) == data(%s)\n", buf, data);
+    printf("buf(%s) == data(%s)...", buf, data);
     assert(!strcmp(buf, data));
-
+    printf("passed\n");
 
     char randombuf[100000];
     char randomdata[100000];
     for (i=0; i<100000; ++i)
 	randomdata[i] = (i % 90) + 32;
-    randomdata[99998] = 0;
-    printf("randomdata %s\n", randomdata);
+    randomdata[99999] = 0;
     sqlfs_proc_write(sqlfs, "/randomdata", randomdata, strlen(randomdata), 0, &fi);
     sleep(1);
     i = sqlfs_proc_read(sqlfs, "/randomdata", randombuf, sizeof(randombuf), 0, &fi);
     randombuf[i] = 0;
-    //assert(!strcmp(randombuf, randomdata));
+    printf("randombuf == randomdata...");
+    assert(!strcmp(randombuf, randomdata));
+    printf("passed\n");
 
     sqlfs_proc_write(sqlfs, "/app/data/file", data, strlen(data), 0, &fi);
     sqlfs_proc_write(sqlfs, "/big/a/b/c/file", data2, 32000, 0, &fi);
 
     i = sqlfs_proc_read(sqlfs, "/big/a/b/c/file", buf, sizeof(buf), 400, &fi);
     assert(buf[97] == '2');
+    sleep(1);
+    printf("Closing database...\n");
     sqlfs_close(sqlfs);
     return 0;
 }
